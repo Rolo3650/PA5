47003ac4fff29e2aa4ee83b1968495327a417582
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2022-10-14 10:18:40 -0500

    consultas 1.0

---
+++ src/DataBase/consultas.js
@@ -0,0 +1,43 @@
+import { con } from "./conexion.js";
+
+export const obtenerTodasLasPublicaciones = () => {
+    const promesa = new Promise((resolve) => {
+        con.query('Select * from mpublicacion', (error, result) => {
+            if (error) {
+                console.error(error);
+            } else {
+                resolve(result);   
+            }
+        })
+    });
+    const publicaciones = promesa;
+    return publicaciones;
+}
+
+export const obtenerComentariosDePublicacion = (id_publicacion) => {
+    const promesa = new Promise((resolve) => {
+        con.query('Select * from mcomentario where id_publicacion = '+ id_publicacion + ';', (error, result) => {
+            if (error) {
+                console.error(error);
+            } else {
+                resolve(result);   
+            }
+        })
+    });
+    const comentarios = promesa;
+    return comentarios;
+}
+
+export const obtenerZona = (id_asentamiento) => {
+    const promesa = new Promise((resolve) => {
+        con.query('Select * from masentamiento natural join czona natural join cestado natural join cmunicipio where id_asentamiento = '+ id_asentamiento + ';', (error, result) => {
+            if (error) {
+                console.error(error);
+            } else {
+                resolve(result);   
+            }
+        })
+    });
+    const zona = promesa;
+    return zona;
+}
