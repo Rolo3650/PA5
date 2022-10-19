9483ba4cefe7e98d457b7153738465d8a1981c88
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2022-10-19 11:56:08 -0500

    consultas e insersiones a clases

--- src/DataBase/consultas.js
+++ src/DataBase/consultas.js
@@ -1,113 +1,120 @@
 import { con } from "./conexion.js";
 
-export const obtenerTodasLasPublicaciones = () => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from mpublicacion', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const publicaciones = promesa;
-    return publicaciones;
-}
+export class Consultas {
 
-export const obtenerComentariosDePublicacion = (id_publicacion) => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from mcomentario where id_publicacion = '+ id_publicacion + ';', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const comentarios = promesa;
-    return comentarios;
-}
+    constructor(){
+        this.con = con;
+    }
 
-export const obtenerEstados = () => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from cestado;', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const estados = promesa;
-    return estados;
-}
+    obtenerTodasLasPublicaciones = () => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from mpublicacion', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const publicaciones = promesa;
+        return publicaciones;
+    }
 
-export const obtenerMunicipios = () => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from cmunicipio;', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const municipios = promesa;
-    return municipios;
-}
+    obtenerComentariosDePublicacion = (id_publicacion) => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from mcomentario where id_publicacion = ' + id_publicacion + ';', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const comentarios = promesa;
+        return comentarios;
+    }
 
-export const obtenerZona = (id_asentamiento) => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from masentamiento natural join czona natural join cestado natural join cmunicipio where id_asentamiento = '+ id_asentamiento + ';', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const zona = promesa;
-    return zona;
-}
+    obtenerEstados = () => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from cestado;', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const estados = promesa;
+        return estados;
+    }
 
-export const obtenerTodasLasZonas = () => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from masentamiento natural join czona natural join cestado natural join cmunicipio;', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const zona = promesa;
-    return zona;
-}
+    obtenerMunicipios = () => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from cmunicipio;', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const municipios = promesa;
+        return municipios;
+    }
 
-export const obtenerImagenesDePublicacion = (id_publicacion) => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from mimagen where id_publicacion = '+ id_publicacion + ';', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const imagenes = promesa;
-    return imagenes;
-}
+    obtenerZona = (id_asentamiento) => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from masentamiento natural join czona natural join cestado natural join cmunicipio where id_asentamiento = ' + id_asentamiento + ';', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const zona = promesa;
+        return zona;
+    }
 
-export const obtenerTiposDeCategorias = () => {
-    const promesa = new Promise((resolve) => {
-        con.query('Select * from ccategoria_publicacion;', (error, result) => {
-            if (error) {
-                console.error(error);
-            } else {
-                resolve(result);   
-            }
-        })
-    });
-    const categorias = promesa;
-    return categorias;
+    obtenerTodasLasZonas = () => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from masentamiento natural join czona natural join cestado natural join cmunicipio;', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const zona = promesa;
+        return zona;
+    }
+
+    obtenerImagenesDePublicacion = (id_publicacion) => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from mimagen where id_publicacion = ' + id_publicacion + ';', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const imagenes = promesa;
+        return imagenes;
+    }
+
+    obtenerTiposDeCategorias = () => {
+        const promesa = new Promise((resolve) => {
+            this.con.query('Select * from ccategoria_publicacion;', (error, result) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    resolve(result);
+                }
+            })
+        });
+        const categorias = promesa;
+        return categorias;
+    }
 }
--- src/DataBase/inserciones.js
+++ src/DataBase/inserciones.js
@@ -1,25 +1,31 @@
 import { con } from "./conexion.js";
 
-export const insertarImagen = (ruta, id_publicacion) => {
-    con.query('INSERT INTO `mimagen` (`ruta`, `id_publicacion`) VALUES (' + ruta + ', ' + id_publicacion + ');', (error, result) => {
-        if (error) {
-            console.error(error);
-        }
-    })
-}
+export class Insersiones {
+    constructor() {
+        this.con = con;
+    }
 
-export const insertarPublicacion = (comentario, fecha, id_usuario, id_categoria, id_asentamiento) => {
-    con.query('INSERT INTO `mydb`.`mpublicacion` (`comentario`, `fecha`, `id_usuario`, `id_categoria`, `id_asentamiento` VALUES ('+comentario+', '+fecha+', '+id_usuario+', '+id_categoria+', '+id_asentamiento+');', (error, result) => {
-        if (error) {
-            console.error(error);
-        }
-    })
-}
+    insertarImagen = (ruta, id_publicacion) => {
+        this.con.query('INSERT INTO `mimagen` (`ruta`, `id_publicacion`) VALUES (' + ruta + ', ' + id_publicacion + ');', (error, result) => {
+            if (error) {
+                console.error(error);
+            }
+        })
+    }
 
-export const insertarComentario = (comentario, fecha, id_publicacion, id_usuario) => {
-    con.query('INSERT INTO `mydb`.`mcomentario` (`comentario`, `fecha`, `id_publicacion`, `id_usuario`) VALUES ('+comentario+', '+fecha+', '+id_publicacion+', '+id_usuario+');', (error, result) => {
-        if (error) {
-            console.error(error);
-        }
-    })
+    insertarPublicacion = (comentario, fecha, id_usuario, id_categoria, id_asentamiento) => {
+        this.con.query('INSERT INTO `mydb`.`mpublicacion` (`comentario`, `fecha`, `id_usuario`, `id_categoria`, `id_asentamiento` VALUES (' + comentario + ', ' + fecha + ', ' + id_usuario + ', ' + id_categoria + ', ' + id_asentamiento + ');', (error, result) => {
+            if (error) {
+                console.error(error);
+            }
+        })
+    }
+
+    insertarComentario = (comentario, fecha, id_publicacion, id_usuario) => {
+        this.con.query('INSERT INTO `mydb`.`mcomentario` (`comentario`, `fecha`, `id_publicacion`, `id_usuario`) VALUES (' + comentario + ', ' + fecha + ', ' + id_publicacion + ', ' + id_usuario + ');', (error, result) => {
+            if (error) {
+                console.error(error);
+            }
+        })
+    }
 }
