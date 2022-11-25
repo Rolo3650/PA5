9a1e402f051d7b0e008d2f28b10c3733cdfcfe39
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2022-11-24 18:14:53 -0600

    build

--- src/DataBase/inserciones.js
+++ src/DataBase/inserciones.js
@@ -18,12 +18,14 @@ export class Inserciones {
         const promesa = new Promise((resolve) => {
             this.#con.query("INSERT INTO `mpersona` (`id_persona`, `nombre`, `appat`, `apmat`, `fecha_nacimiento`, `id_sexo`, `id_asentamiento`) VALUES ('" + id_persona + "', '" + persona.obtenerNombre() + "', '" + persona.obtenerAppat() + "', '" + persona.obtenerApmat() + "', '" + persona.obtenerFechaNacimiento() + "', '" + persona.obtenerIdSexo() + "', '" + persona.obtenerIdAsentamiento() + "');", (error, result) => {
                 if (error) {
+                    console.log(error)
                     resolve(false);
                 } else {
                 }
             })
             this.#con.query("INSERT INTO `musuario` (`id_usuario`, `correo`, `contrasenia`, `id_persona`, `id_tipo`) VALUES ('" + id_persona + "', '" + usuario.obtenerCorreo() + "', '" + usuario.obtenerContrasenia() + "', '" + id_persona + "', '1');", (error, result) => {
                 if (error) {
+                    console.log(error)
                     resolve(false);
                 } else {
                     resolve(true);
